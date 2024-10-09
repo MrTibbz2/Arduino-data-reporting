a22eb3ff94260b244e5929d956db50cd46182e75
Vipralix <toastedbread275@gmail.com>
2024-10-10 10:39:30 +1100

    Update and rename RF24 Recieve to RX.ino

--- RX.ino
+++ RX.ino
@@ -1,15 +1,7 @@
-    /* By Jeremy S. Cook 3/21/2019
-    * 
-    * Code Modified from: Arduino Wireless Communication Tutorial
-    * Example 1 - Receiver Code
-    * by Dejan Nedelkovski, www.HowToMechatronics.com
-    * 
-    * Library: TMRh20/RF24, https://github.com/nRF24/RF24
-    */
     #include <SPI.h>
     #include <nRF24L01.h>
     #include <RF24.h>
-    RF24 radio(7, 8); // CE, CSN
+    RF24 radio(9, 10); // CE, CSN
     const byte address[6] = "00001";
     void setup() {
       Serial.begin(9600);
